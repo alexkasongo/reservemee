f439ce926f1db2d803e86b763ff3deb21146bf3d
Aleko <alex.kasongolenge@yahoo.com>
2020-09-29 15:38:53 -0700

    calendar timed events now working

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -263,7 +263,27 @@ export default {
         selectedElement: null,
         selectedOpen: false,
         events: [],
-        dialog: false
+        dialog: false,
+        names: [],
+        colors: [
+            'blue',
+            'indigo',
+            'deep-purple',
+            'cyan',
+            'green',
+            'orange',
+            'grey darken-1'
+        ],
+        names: [
+            'Meeting',
+            'Holiday',
+            'PTO',
+            'Travel',
+            'Event',
+            'Birthday',
+            'Conference',
+            'Party'
+        ]
     }),
     computed: {
         title() {
@@ -336,6 +356,7 @@ export default {
             let events = [];
             // loop through and push events on each itteration
             snapshot.forEach((doc) => {
+                console.log(`Calendar.vue - 359 - 🍺`, doc.data());
                 let appData = [];
                 appData.id = doc.id;
                 appData.color = doc.data().color;
@@ -343,6 +364,8 @@ export default {
                 appData.end = doc.data().end.toDate();
                 appData.name = doc.data().name;
                 appData.start = doc.data().start.toDate();
+                // Must include timed:boolean else timed events will not be displayed accordingly
+                appData.timed = doc.data().timed;
                 events.push(appData);
             });
             console.log(`Calendar.vue - 143 - 🥶`, events);
@@ -425,8 +448,39 @@ export default {
             nativeEvent.stopPropagation();
         },
         updateRange({ start, end }) {
-            this.start = start;
-            this.end = end;
+            // console.log(`Calendar.vue - 428 - 🥛`, start, end);
+            // this.start = start;
+            // this.end = end;
+            const events = [];
+
+            const min = new Date(`${start.date}T00:00:00`);
+            const max = new Date(`${end.date}T23:59:59`);
+            const days = (max.getTime() - min.getTime()) / 86400000;
+            const eventCount = this.rnd(days, days + 20);
+
+            for (let i = 0; i < eventCount; i++) {
+                const allDay = this.rnd(0, 3) === 0;
+                const firstTimestamp = this.rnd(min.getTime(), max.getTime());
+                const first = new Date(
+                    firstTimestamp - (firstTimestamp % 900000)
+                );
+                const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
+                const second = new Date(first.getTime() + secondTimestamp);
+
+                events.push({
+                    name: this.names[this.rnd(0, this.names.length - 1)],
+                    start: first,
+                    end: second,
+                    color: this.colors[this.rnd(0, this.colors.length - 1)],
+                    timed: !allDay
+                });
+            }
+
+            // this.events = events;
+            console.log(`Calendar.vue - 477 - 🧃`, events);
+        },
+        rnd(a, b) {
+            return Math.floor((b - a + 1) * Math.random()) + a;
         },
         nth(d) {
             return d > 3 && d < 21
