78e3379a4d218bc5b7063f44c6105d4e30adb21a
Aleko <alex.kasongolenge@yahoo.com>
2020-11-04 14:05:10 -0800

    contribution

--- plugins/filters.js
+++ plugins/filters.js
@@ -6,7 +6,8 @@ export function capitalize(value) {
     let initialFormat = value.replace(/-/g, ' ');
     let formattedValue = initialFormat.toLowerCase().split(' ');
     for (var i = 0; i < formattedValue.length; i++) {
-        // You do not need to check if i is larger than formattedValue length, as your for does that for you
+        // You do not need to check if i is larger than formattedValue length, 
+        // as your for does that for you
         // Assign it back to the array.
         formattedValue[i] =
             formattedValue[i].charAt(0).toUpperCase() +
