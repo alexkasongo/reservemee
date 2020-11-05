bfbb416ada5abce536899a1a9b4af2817c7b43f5
Aleko <alex.kasongolenge@yahoo.com>
2020-11-05 00:37:56 -0800

    slider is now working using dynamic data

--- pages/playground/landing.vue
+++ pages/playground/landing.vue
@@ -1,54 +1,64 @@
 <template>
-    <div class="margin wrapper">
+    <div class="margin">
         <splide
-            :slides="slides"
-            :options="options"
+            :options="secondaryOptions"
             ref="secondary"
             @splide:moved="moved"
+            :slides="slides"
         >
             <splide-slide v-for="slide in slides" :key="slide.id">
-                <img :src="slide.src" />
+                <div class="slider-image">
+                    <!-- <img :src="slide.src" /> -->
+                    <div>{{ slide.color }}</div>
+                </div>
             </splide-slide>
         </splide>
 
-        <splide :options="primaryOptions" ref="primary">
+        <splide :options="primaryOptions" ref="primary" :slides="slides">
             <splide-slide v-for="slide in slides" :key="slide.id">
-                <img :src="slide.src" />
+                <!-- <img :src="slide.src" /> -->
+                <div class="slider-image">
+                    <!-- <img :src="slide.src" /> -->
+                    <div>{{ slide.color }}</div>
+                </div>
             </splide-slide>
         </splide>
     </div>
 </template>
 
 <script>
+import { db } from '@/plugins/firebase';
+
 export default {
     data() {
         return {
-            slides: [
-                { src: 'https://via.placeholder.com/150/FFADAD' },
-                { src: 'https://via.placeholder.com/150/264653' },
-                { src: 'https://via.placeholder.com/150/2A9D8F' },
-                { src: 'https://via.placeholder.com/150/E9C46A' },
-                { src: 'https://via.placeholder.com/150/F4A261' },
-                { src: 'https://via.placeholder.com/150/E76F51' },
-                { src: 'https://via.placeholder.com/150/E63946' },
-                { src: 'https://via.placeholder.com/150/F1FAEE' },
-                { src: 'https://via.placeholder.com/150/A8DADC' },
-                { src: 'https://via.placeholder.com/150/457B9D' }
-            ],
+            slides: [],
+            // slides: [
+            //     { src: 'https://via.placeholder.com/150/FFADAD' },
+            //     { src: 'https://via.placeholder.com/150/264653' },
+            //     { src: 'https://via.placeholder.com/150/2A9D8F' },
+            //     { src: 'https://via.placeholder.com/150/E9C46A' },
+            //     { src: 'https://via.placeholder.com/150/F4A261' },
+            //     { src: 'https://via.placeholder.com/150/E76F51' },
+            //     { src: 'https://via.placeholder.com/150/E63946' },
+            //     { src: 'https://via.placeholder.com/150/F1FAEE' },
+            //     { src: 'https://via.placeholder.com/150/A8DADC' },
+            //     { src: 'https://via.placeholder.com/150/457B9D' }
+            // ],
             primaryOptions: {
                 type: 'loop',
-                width: 800,
+                // width: 800,
                 pagination: false,
-                arrows: false
+                // arrows: false
+                fixedHeight: 185
             },
-            options: {
+            secondaryOptions: {
                 type: 'slide',
                 rewind: true,
-                width: 800,
-                gap: '1rem',
+                // width: 800,
                 pagination: false,
-                fixedWidth: 110,
-                fixedHeight: 70,
+                fixedWidth: 150,
+                fixedHeight: 185,
                 cover: true,
                 focus: 'center',
                 isNavigation: true
@@ -57,18 +67,52 @@ export default {
     },
 
     mounted() {
-        // Attept to add a slide.
-        // setTimeout(() => {
-        //     this.slides.push({
-        //         src: 'https://via.placeholder.com/150'
-        //     });
-        // }, 1);
+        this.getEvents();
         this.$refs.primary.sync(this.$refs.secondary.splide);
     },
     methods: {
         moved(splide, newIndex) {
             // console.log('moved', newIndex);
+        },
+        selectedDate(data) {
+            console.log(`landing.vue - 73 - you clicked me 🌈`);
+        },
+
+        async getEvents() {
+            let snapshot = await db
+                .collection('jKWLSv7dhUfUIDXWalNfnDZKX2I2')
+                .get();
+            let events = [];
+            // loop through and push events on each itteration
+            snapshot.forEach((doc) => {
+                let appData = [];
+                appData.id = doc.id;
+                appData.color = doc.data().color;
+                appData.details = doc.data().details;
+                appData.end = doc.data().end.toDate();
+                appData.name = doc.data().name;
+                appData.start = doc.data().start.toDate();
+                // Must include timed:boolean else timed events will not be displayed accordingly
+                appData.timed = doc.data().timed;
+                events.push(appData);
+            });
+            this.slides = events;
+            console.log(`landing.vue - 91 - 🌕`, events);
         }
     }
 };
 </script>
+
+<style lang="scss" scoped>
+.slider-image {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: lemonchiffon;
+    // width: 150px;
+    // height: 100px;
+    height: 100%;
+    width: 100%;
+}
+</style>
