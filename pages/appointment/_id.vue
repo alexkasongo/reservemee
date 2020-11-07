45b42d0f8708d4b5edebc80e1428883e0136c651
Aleko <alex.kasongolenge@yahoo.com>
2020-11-06 16:03:02 -0800

    removed dead code

--- pages/appointment/_id.vue
+++ pages/appointment/_id.vue
@@ -151,17 +151,6 @@
                 <!-- SERVICE CARD END-->
 
                 <v-list-item class="grow">
-                    <!-- <v-list-item-avatar color="grey darken-3">
-                        <v-img
-                            class="elevation-6"
-                            alt=""
-                            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-                        ></v-img>
-                    </v-list-item-avatar>
-
-                    <v-list-item-content>
-                        <v-list-item-title>Evan You</v-list-item-title>
-                    </v-list-item-content> -->
                     <v-row align="center" justify="end">
                         <v-btn
                             rounded
