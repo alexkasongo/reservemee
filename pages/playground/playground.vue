9cfff6dc168ee0b42d6e948961c4ad5f42de7b89
Aleko <alex.kasongolenge@yahoo.com>
2020-11-11 21:00:19 -0800

    added user module

--- pages/playground/playground.vue
+++ pages/playground/playground.vue
@@ -1,20 +1,22 @@
 <template>
-    <ul>
-        <li v-for="todo in todos" :key="todo.text">
-            <input
-                :checked="todo.done"
-                @change="toggle(todo)"
-                type="checkbox"
-            />
-            <span :class="{ done: todo.done }">{{ todo.text }}</span>
-        </li>
-        <li>
-            <input
-                @keyup.enter="addTodo"
-                placeholder="What needs to be done?"
-            />
-        </li>
-    </ul>
+    <div class="test">
+        <ul>
+            <li v-for="todo in todos" :key="todo.text">
+                <input
+                    :checked="todo.done"
+                    @change="toggle(todo)"
+                    type="checkbox"
+                />
+                <span :class="{ done: todo.done }">{{ todo.text }}</span>
+            </li>
+            <li>
+                <input
+                    @keyup.enter="addTodo"
+                    placeholder="What needs to be done?"
+                />
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -38,8 +40,11 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 .done {
     text-decoration: line-through;
+}
+.test {
+    margin: 100px;
 }
 </style>
