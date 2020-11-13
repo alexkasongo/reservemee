f52100cba02e3641a320c8e344b714f7386d2fa4
Aleko <alex.kasongolenge@yahoo.com>
2020-11-12 16:13:01 -0800

    cleanup

--- store/chat.js
+++ store/chat.js
@@ -1,8 +1,6 @@
 import * as firebase from 'firebase/app';
-import 'firebase/auth';
 import 'firebase/database';
-import 'firebase/storage';
-import 'firebase/functions';
+
 
 export const state = () => ({
     messages: '',
