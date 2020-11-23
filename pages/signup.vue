e6fe8c7693b976b3ede0e7daff913bc1425162e6
Aleko <alex.kasongolenge@yahoo.com>
2020-11-22 18:11:05 -0800

    signup form style updated and validation added

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
+        <div v-if="loading">
+            <p>Loading...</p>
+        </div>
         <div class="signup">
             <div class="signup__left"></div>
             <div class="signup__right">
-                <div
-                    v-if="!loading"
-                    class="d-flex flex-row justify-content-center"
-                >
-                    <div class="card">
+                <div v-if="!loading" class="col-6">
+                    <!-- <div class="card">
                         <div class="card-header">Sign Up</div>
 
                         <div class="card-body">
@@ -70,7 +70,66 @@
                                 >
                             </form>
                         </div>
-                    </div>
+                    </div> -->
+
+                    <validation-observer ref="observer" v-slot="{ invalid }">
+                        <form @submit.prevent="onSubmit">
+                            <!-- Name -->
+                            <validation-provider
+                                v-slot="{ errors }"
+                                name="Name"
+                                rules="required|max:10"
+                            >
+                                <v-text-field
+                                    v-model="name"
+                                    :counter="10"
+                                    :error-messages="errors"
+                                    label="Name"
+                                    required
+                                ></v-text-field>
+                            </validation-provider>
+                            <!-- Name End -->
+                            <!-- Email -->
+                            <validation-provider
+                                v-slot="{ errors }"
+                                name="email"
+                                rules="required|email"
+                            >
+                                <v-text-field
+                                    v-model="email"
+                                    :error-messages="errors"
+                                    label="E-mail"
+                                    required
+                                ></v-text-field>
+                            </validation-provider>
+                            <!-- Email End -->
+                            <!-- Password -->
+                            <validation-provider
+                                v-slot="{ errors }"
+                                name="Password"
+                                rules="required|max:10"
+                            >
+                                <v-text-field
+                                    type="password"
+                                    v-model="password"
+                                    :counter="10"
+                                    :error-messages="errors"
+                                    label="Password"
+                                    required
+                                ></v-text-field>
+                            </validation-provider>
+                            <!-- Password End -->
+                            <!-- Submit Button -->
+                            <v-btn
+                                class="mr-4"
+                                type="submit"
+                                :disabled="invalid"
+                            >
+                                submit
+                            </v-btn>
+                            <v-btn @click="clear"> clear </v-btn>
+                        </form>
+                    </validation-observer>
                 </div>
             </div>
         </div>
@@ -79,16 +138,45 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import { required, email, max } from 'vee-validate/dist/rules';
+import {
+    extend,
+    ValidationObserver,
+    ValidationProvider,
+    setInteractionMode
+} from 'vee-validate';
+
+setInteractionMode('eager');
+
+extend('required', {
+    ...required,
+    message: '{_field_} can not be empty'
+});
+
+extend('max', {
+    ...max,
+    message: '{_field_} may not be greater than {length} characters'
+});
+
+extend('email', {
+    ...email,
+    message: 'Email must be valid'
+});
 
 export default {
-    data() {
-        return {
-            name: '',
-            phone: '',
-            email: '',
-            password: ''
-        };
+    components: {
+        ValidationProvider,
+        ValidationObserver
     },
+    data: () => ({
+        name: '',
+        phone: '',
+        email: '',
+        password: '',
+        name: '',
+        email: '',
+        checkbox: null
+    }),
     computed: {
         ...mapGetters({
             errors: 'signupError'
@@ -106,6 +194,17 @@ export default {
                 password: this.password
             };
             this.signup(signupDetails);
+        },
+        // Vee Validate
+        submit() {
+            this.$refs.observer.validate();
+        },
+        clear() {
+            this.name = '';
+            this.email = '';
+            this.select = null;
+            this.checkbox = null;
+            this.$refs.observer.reset();
         }
     },
     mounted() {
@@ -127,7 +226,7 @@ export default {
         justify-content: center;
         align-items: center;
         width: 65%;
-        background-color: #00897b;
+        // background-color: #00897b;
     }
 }
 </style>
