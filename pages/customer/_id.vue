f498b52daf1264c0a4fb8a325dee0bc6e373aa93
Aleko <alex.kasongolenge@yahoo.com>
2020-12-05 01:06:58 -0800

    fixing messages code

--- pages/customer/_id.vue
+++ pages/customer/_id.vue
@@ -3,32 +3,41 @@
         <h1>Messages</h1>
         <!-- <p>{{ test }}</p> -->
         <div class="card">
-            <!-- <div class="card" v-if="currentUserMessages"> -->
             <h2 class="deep-purple-text center">
-                {{ this.sender }}
+                {{ sender.name }}
             </h2>
-            <!-- <ul class="comments collection">
-                <li v-for="(comment, index) in comments" :key="index">
-                    <div class="deep-purple-text">{{ comment.from }}</div>
+            <ul class="comments collection">
+                <li>
+                    <div class="deep-purple-text">{{ sender.name }}</div>
                     <div class="grey-text text-darken-2">
-                        {{ comment.message }}
+                        {{ sender.message }}
                     </div>
                 </li>
             </ul>
-            <form @submit.prevent="onSubmit">
+            <div class="card" v-if="currentUserMessages">
+                <ul class="comments collection">
+                    <li v-for="(comment, index) in comments" :key="index">
+                        <div class="deep-purple-text">{{ comment.from }}</div>
+                        <div class="grey-text text-darken-2">
+                            {{ comment.message }}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <form @submit.prevent="onReply">
                 <div class="field">
                     <label for="comment">Write Message</label>
                     <v-text-field
                         type="text"
                         name="comment"
-                        v-model="newComment"
+                        v-model="newReply"
                     ></v-text-field>
                     <p v-if="feedback" class="red-text center">
                         {{ feedback }}
                     </p>
                 </div>
                 <v-btn type="submit">Send</v-btn>
-            </form> -->
+            </form>
         </div>
     </div>
 </template>
@@ -40,7 +49,7 @@ import { mapState, mapGetters } from 'vuex';
 export default {
     data: () => ({
         profile: null,
-        newComment: null,
+        newReply: null,
         feedback: null,
         // uid: 'RGfjW6W4YMUgClckhJE5PccAtSF3',
         messages: null
@@ -50,51 +59,62 @@ export default {
             user: 'user'
             // loadedComments: 'chat/comments'
         }),
+        loadedComments() {
+            return this.loadedChats.comments;
+        },
         sender() {
-            return this.loadedMessages.messages;
+            return this.loadedChats.messages[0];
         },
         currentUserMessages() {
-            const messages = this.comments.filter((res) => {
-                return res.userId === this.$route.params.id;
-            });
-            return messages[0];
+            if (!this.comments === null) {
+                const messages = this.comments.filter((res) => {
+                    return res.userId === this.$route.params.id;
+                });
+                return messages[0];
+            }
         },
         comments() {
             const clonedComments = [];
-            const comments = this.loadedComments;
-            comments.forEach((comment) => {
-                clonedComments.push({
-                    ...comment
+            if (!this.loadedComments.length <= 0) {
+                const comments = this.loadedComments;
+                comments.forEach((comment) => {
+                    clonedComments.push({
+                        ...comment
+                    });
                 });
-            });
-            let filtered = clonedComments.filter(
-                (res) => res.userId === this.$route.params.id
-            );
-            return filtered;
+                let filtered = clonedComments.filter(
+                    (res) => res.userId === this.$route.params.id
+                );
+                return filtered;
+            } else {
+                return null;
+            }
         },
         ...mapState({
             user: 'user',
-            loadedMessages: 'chat'
+            loadedChats: 'chat'
         })
     },
     created() {
         this.$store.dispatch('chat/loadComments', this.user.uid);
-        console.log(`_id.vue - 82 - 🌈`, this.loadedMessages);
+        console.log(`_id.vue - 82 - 😳`, this.loadedChats.messages);
+        console.log(`_id.vue - 82 - 🌈`, this.user);
     },
     methods: {
         //NOTE  this is adding new comment
-        onSubmit() {
-            if (this.newComment) {
+        onReply() {
+            if (this.newReply) {
                 const createdMessage = {
-                    to: this.$route.params.name,
+                    to: this.sender.name,
                     from: this.user.name,
                     userId: this.$route.params.id,
-                    storeId: this.uid,
-                    message: this.newComment
+                    storeId: this.user.uid,
+                    message: this.newReply
                 };
-                this.$store.dispatch('chat/sendPrivateMessage', createdMessage);
-                this.newComment = null;
-                this.feedback = null;
+                console.log(`_id.vue - 113 - 🍎`, createdMessage);
+                // this.$store.dispatch('chat/sendPrivateMessage', createdMessage);
+                // this.newReply = null;
+                // this.feedback = null;
             } else {
                 this.feedback = 'You must enter a comment to add it';
             }
@@ -115,8 +135,5 @@ export default {
 .view-profile li {
     padding: 10px;
     border-bottom: 1px solid #eee;
-}
-body {
-    background: #ddd;
 }
 </style>
