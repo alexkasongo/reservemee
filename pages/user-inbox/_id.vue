0123ee3e2b177f71b06c6a6db178b8b0037e65e5
Aleko <alex.kasongolenge@yahoo.com>
2020-12-08 01:13:41 -0800

    messages logic working

--- pages/user-inbox/_id.vue
+++ pages/user-inbox/_id.vue
@@ -1,49 +1,68 @@
 <template>
-    <div>
+    <div class="inbox container">
         <!-- PREVIEW -->
-        <div>messages</div>
-        <v-card elevation="0" class="mx-auto rounded-xl">
-            <template slot="progress">
-                <v-progress-linear
-                    color="deep-purple"
-                    height="10"
-                    indeterminate
-                ></v-progress-linear>
-            </template>
 
-            <v-card-title>Services</v-card-title>
+        <div class="inbox__left">
+            <v-card class="mx-auto" width="100%" tile>
+                <v-navigation-drawer width="100%" permanent>
+                    <v-list>
+                        <v-list-item>
+                            <v-list-item-avatar>
+                                <v-img
+                                    src="https://via.placeholder.com/120"
+                                ></v-img>
+                            </v-list-item-avatar>
+                        </v-list-item>
 
-            <!-- <v-card-text>
-                <div class="my-4 subtitle-1">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Select Category</label>
-                        <select
-                            @change="onChange($event)"
-                            class="form-control"
-                            required
+                        <v-list-item>
+                            <v-list-item-content>
+                                <v-list-item-title class="title">
+                                    {{ user.name }}
+                                </v-list-item-title>
+                                <v-list-item-subtitle>{{
+                                    user.email
+                                }}</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list>
+                    <v-divider></v-divider>
+                    <v-list nav>
+                        <v-list-item-group
+                            v-model="selectedItem"
+                            color="primary"
                         >
-                            <option value="all" selected>All</option>
-                            <option
-                                v-for="(messenger, index) in allMessages"
-                                :key="index"
+                            <v-list-item
+                                v-for="(messenger, i) in allMessages"
+                                :key="i"
                             >
-                                {{ messenger.message | capitalize }}
-                            </option>
-                        </select>
-                    </div>
-                </div>
-            </v-card-text> -->
-            <div v-for="(messenger, index) in allMessages"
-                                :key="index">
-                <p @click="onChange(messenger)">{{ messenger.message }}</p>
-            </div>
-        </v-card>
+                                <v-list-item-avatar>
+                                    <v-img
+                                        :src="messenger.storeOwnerImage"
+                                    ></v-img>
+                                </v-list-item-avatar>
+
+                                <v-list-item-content
+                                    @click="onChange(messenger)"
+                                >
+                                    <v-list-item-title
+                                        v-text="messenger.message"
+                                    ></v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list>
+                </v-navigation-drawer>
+            </v-card>
+        </div>
         <!-- PREVIEW END -->
+
         <!-- MESSAGE -->
-        <div v-for="(message, index) in messages" :key="index">
-            <p>
-                <p>{{ message }}</p>
-            </p>
+        <div class="inbox__right">
+            <v-card>
+                <div v-for="(message, index) in messages" :key="index">
+                    <p>{{ message }}</p>
+                </div>
+            </v-card>
         </div>
         <!-- MESSAGE -->
     </div>
@@ -54,11 +73,22 @@ import { mapState, mapGetters, mapActions } from 'vuex';
 
 export default {
     data: () => ({
-        messages: []
+        messages: [],
+        selectedItem: 0,
+        items: [
+            { text: 'My Files', icon: 'mdi-folder' },
+            { text: 'Shared with me', icon: 'mdi-account-multiple' },
+            { text: 'Starred', icon: 'mdi-star' },
+            { text: 'Recent', icon: 'mdi-history' },
+            { text: 'Offline', icon: 'mdi-check-circle' },
+            { text: 'Uploads', icon: 'mdi-upload' },
+            { text: 'Backups', icon: 'mdi-cloud-upload' }
+        ]
     }),
     computed: {
         ...mapGetters({
-            replies: 'chat/replies'
+            replies: 'chat/replies',
+            user: 'user'
         }),
         ...mapState({
             allMessagesData: 'chat'
@@ -76,7 +106,6 @@ export default {
             loadReplies: 'chat/loadReplies'
         }),
         onChange(e) {
-            console.log(`_id.vue - 75 - variable`, e.messagePreviewId);
             // replace empty space with dash and transform to lowercase as value Array.filter()
             // is case sensitive
             const value = e.messagePreviewId;
@@ -108,12 +137,22 @@ export default {
     mounted() {
         this.loadMessages(this.$route.params.id).then(() => {});
         this.loadReplies(this.$route.params.id).then(() => {
-            this.messages = this.replies;
+            // this.messages = this.replies;
             console.log(`_id.vue - 108 - variable`, this.messages);
         });
     }
 };
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.inbox {
+    display: flex;
+    &__left {
+        width: 40%;
+        margin: 0 10px 0 0;
+    }
+    &__right {
+        width: 60%;
+    }
+}
 </style>
