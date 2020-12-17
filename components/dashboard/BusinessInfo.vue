48540e83a4af49ac2cf5bf7aa8ef0b32c7ce219d
Alex Kasongo <alex.kasongolenge@yahoo.com>
2020-12-16 16:35:47 -0800

    complete

--- components/dashboard/BusinessInfo.vue
+++ components/dashboard/BusinessInfo.vue
@@ -58,7 +58,13 @@
                     </p>
                 </div>
             </div>
-            <v-card class="mx-auto" color="#26c6da" dark max-width="400">
+            <v-card
+                class="mx-auto"
+                color="teal darker-1"
+                elevation="0"
+                dark
+                max-width="400"
+            >
                 <v-card-actions>
                     <v-list-item class="grow">
                         <v-list-item-avatar color="grey darken-3">
@@ -76,62 +82,25 @@
                         </v-list-item-content>
 
                         <v-row align="center" justify="end">
-                            <v-btn
-                                @click="$route.push(`${storeProfile.facebook}`)"
-                                icon
-                            >
-                                <v-icon>mdi-facebook</v-icon>
-                            </v-btn>
-
-                            <v-btn icon>
-                                <v-icon>mdi-instagram</v-icon>
-                            </v-btn>
-
-                            <v-btn icon>
-                                <v-icon>mdi-twitter</v-icon>
-                            </v-btn>
+                            <a :href="storeProfile.facebook" target="_blank">
+                                <v-btn icon>
+                                    <v-icon>mdi-facebook</v-icon>
+                                </v-btn>
+                            </a>
+                            <a :href="storeProfile.instagram" target="_blank">
+                                <v-btn icon>
+                                    <v-icon>mdi-instagram</v-icon>
+                                </v-btn>
+                            </a>
+                            <a :href="storeProfile.twitter" target="_blank">
+                                <v-btn icon>
+                                    <v-icon>mdi-twitter</v-icon>
+                                </v-btn>
+                            </a>
                         </v-row>
                     </v-list-item>
                 </v-card-actions>
             </v-card>
-            <!-- <ul class="social-links list-inline mt-4 mb-0">
-                <li class="list-inline-item">
-                    <a
-                        title
-                        data-placement="top"
-                        data-toggle="tooltip"
-                        class="tooltips"
-                        href
-                        data-original-title="Facebook"
-                    >
-                        <i class="fa text-dark fa-facebook"></i>
-                    </a>
-                </li>
-                <li class="list-inline-item">
-                    <a
-                        title
-                        data-placement="top"
-                        data-toggle="tooltip"
-                        class="tooltips"
-                        href
-                        data-original-title="Twitter"
-                    >
-                        <i class="fa text-dark fa-twitter"></i>
-                    </a>
-                </li>
-                <li class="list-inline-item">
-                    <a
-                        title
-                        data-placement="top"
-                        data-toggle="tooltip"
-                        class="tooltips"
-                        href
-                        data-original-title="Skype"
-                    >
-                        <i class="fa text-dark fa-skype"></i>
-                    </a>
-                </li>
-            </ul> -->
         </div>
         <div v-else>
             <v-alert
