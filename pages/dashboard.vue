81606ac1a51f9b8dc220bf71db3df35e2677120e
Alex Kasongo <alex.kasongolenge@yahoo.com>
2020-12-17 20:18:00 -0800

    updated dashboard design

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -10,20 +10,20 @@
             <div class="row">
                 <div class="col-xl-4">
                     <div>
-                        <h2 class="header-title mt-5">Bio</h2>
+                        <div class="display-1 mt-5 mb-5">Bio</div>
                         <!-- Personal-Information -->
                         <BusinessInfo />
                         <!-- Personal-Information -->
                         <div v-if="user">
-                            <h2
+                            <div
                                 v-if="
                                     categories.length <= 0 ||
                                     !user.emailVerified
                                 "
-                                class="header-title mt-0"
+                                class="display-1 mt-10 mb-5"
                             >
                                 Todos
-                            </h2>
+                            </div>
                         </div>
                         <div v-if="user">
                             <div>
@@ -42,7 +42,7 @@
                     </div>
 
                     <div>
-                        <h2 class="header-title mt-5">Messages</h2>
+                        <div class="display-1 mt-10 mb-5">Messages</div>
                         <!-- Messages -->
                         <StoreMessages />
                         <!-- Messages -->
@@ -57,13 +57,13 @@
                         <Scheduling class="services" />
                         <UpcomingEvents />
                     </div> -->
-                    <h2 class="header-title mt-0">Calendar</h2>
+                    <div class="display-1 mt-10 mb-5">Calendar</div>
                     <!-- <v-card elevation="2"> -->
                     <Calendar />
                     <!-- </v-card>s -->
 
                     <div>
-                        <h2 class="header-title mt-0">Services</h2>
+                        <div class="display-1 mt-10 mb-5">Services</div>
                         <!-- ManageServices -->
                         <ManageServices v-if="categories.length !== 0" />
                         <!-- ManageServices -->
