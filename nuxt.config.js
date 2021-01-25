168f17de68b42e628cc082118bce3d8dc83b02ef
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-01-24 21:58:37 -0800

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,6 @@ export default {
         '~/plugins/firebase.js',
         '~plugins/filters.js',
         { src: '~/plugins/localStorage.js', ssr: false },
-        { src: '~/plugins/vueSplide.js', ssr: false },
         { src: '~/plugins/vee-validate.js', ssr: true }
     ],
     /*
