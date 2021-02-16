256e358311abff010bab3e2b9358c3776e8f82d9
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-02-15 22:38:38 -0800

    removed more dead

--- store/cart.js
+++ store/cart.js
@@ -6,27 +6,6 @@ export const getters = {};
 
 export const actions = {
     async addToCart({ commit }, payload) {
-        console.log(`cart.js - 9 - 🙏🏾`, payload);
-
-        // create order object
-        // const order = await {
-        //     id: payload.id,
-        //     note: payload.note,
-        //     serviceCategory: payload.service.category,
-        //     serviceDescription: payload.service.description,
-        //     serviceId: payload.service.id,
-        //     serviceName: payload.service.name,
-        //     servicePrice: payload.service.price,
-        //     serviceImage: payload.service.serviceImage,
-        //     serviceUserId: payload.service.userId,
-        //     eventBooked: payload.event.booked,
-        //     eventColor: payload.event.color,
-        //     eventDetails: payload.event.details,
-        //     eventEnd: payload.event.end,
-        //     eventName: payload.event.name,
-        //     eventStart: payload.event.start,
-        //     eventTimed: payload.event.timed
-        // };
         commit('SET_CART', payload);
         this.$swal({
             toast: true,
