c2523e3d4305bdedff3bdf8bbeda6d82bed1b9f9
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-02-15 22:37:37 -0800

    verification page updated to beufy

--- pages/verification.vue
+++ pages/verification.vue
@@ -2,54 +2,70 @@
     <div class="container">
         <div v-if="userLoaded.emailVerified" class="w-100 mt-2">
             <div class="card">
-                <div class="card-body">
-                    <h3>Your email address is verified.</h3>
-                    <p>Verified: {{ userLoaded.emailVerified }}</p>
-                    <button
-                        @click="goToDashboard"
-                        type="button"
-                        class="btn btn-block btn-primary"
-                    >
-                        Go to Dashboard
-                    </button>
+                <div class="card-content">
+                    <div class="card-body">
+                        <h3 class="title is-3">
+                            Your email address is verified
+                        </h3>
+                        <b-button
+                            @click="goToDashboard"
+                            type="is-primary"
+                            class="btn btn-block btn-primary"
+                        >
+                            Go to Dashboard
+                        </b-button>
+                    </div>
                 </div>
             </div>
         </div>
         <div v-if="verification" class="w-100 mt-2">
             <div class="card">
-                <div class="card-body">
-                    <h3>A verification email has been sent to your mailbox</h3>
-                    <p>Verify your email address before signing in.</p>
-                    <div
-                        @click="signout"
-                        type="button"
-                        color="teal darker-1"
-                        dark
-                    >
-                        Signin
-                    </div>
+                <div class="card-content">
+                    <h3 class="title is-3">
+                        A verification email has been sent to your mailbox
+                    </h3>
+                    <h2 class="subtitle">
+                        Verify your email address before signing in.
+                    </h2>
+                    <b-field>
+                        <b-button
+                            @click="signout"
+                            type="is-primary"
+                            color="teal darker-1"
+                            dark
+                        >
+                            Signin
+                        </b-button>
+                    </b-field>
                 </div>
             </div>
         </div>
         <div v-if="!loading" class="d-flex flex-row justify-content-center">
             <div v-if="!userLoaded.emailVerified" class="w-100 mt-2">
-                <div class="display-1 p-3">Verify Email</div>
                 <div class="card-body">
-                    <form @submit.prevent="onSubmit">
-                        <div class="form-group">
-                            <v-text-field
+                    <form>
+                        <h1 class="title is-3">Verify Email</h1>
+                        <b-field>
+                            <b-input
                                 type="text"
                                 placeholder="Email"
                                 v-model="email"
                                 required
-                            ></v-text-field>
-                        </div>
-                        <div v-if="errors" class="form-group text-muted">
+                            ></b-input>
+                        </b-field>
+                        <div v-if="errors" class="text-muted">
                             {{ errors.message }}
                         </div>
-                        <div type="submit" color="teal darker-1" dark>
-                            Submit
-                        </div>
+                        <b-field>
+                            <b-button
+                                type="is-primary"
+                                @click="onSubmit"
+                                color="teal darker-1"
+                                dark
+                            >
+                                Submit
+                            </b-button>
+                        </b-field>
                     </form>
                 </div>
             </div>
