d373e283f9c7bbdd45e3a84dc13042ec98414b64
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-03-08 16:30:14 -0800

    removed dead code

--- pages/settings.vue
+++ pages/settings.vue
@@ -192,12 +192,10 @@ export default {
     display: flex;
     flex-direction: column;
     width: 100%;
-    // height: 100%;
     min-height: 100vh;
     .sidebar-layout {
         display: flex;
         flex-direction: row;
-        // height: calc(100vh - 56px);
         min-height: 100vh;
     }
 }
